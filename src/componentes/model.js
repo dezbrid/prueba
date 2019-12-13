98d7ee9f4995c7afa7d4ca52b38b7057512e4037
dezbrid <dezbrid@gmail.com>
2019-12-12 20:48:45 -0500

    avance

---
+++ src/componentes/model.js
@@ -0,0 +1,14 @@
+export const Form = [
+    {
+        id: FIRST_NAME,
+        validationMsgs: [{
+            empty: "This field cannot be empty.",
+            valid: "Enter a valid name."
+        }],
+        error: false,
+        helperText: "",
+        value: "",
+        label: "First name",
+        maxLength: 25,
+        
+    }];
---
+++ src/componentes/view.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    
+    Paper,
+   
+} from "@material-ui/core";
+
+
+function view() {
+    return (
+        <Paper>
+            
+        </Paper>
+    );
+}
+
+export default view;
