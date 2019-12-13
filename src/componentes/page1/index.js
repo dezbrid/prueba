eb6ebdfec9283994340ae1d6a48909e15fb0eee6
Daniel <danielz.micalisoft@gmail.com>
2019-12-13 11:36:56 -0500

    .

--- src/componentes/page1/index.js
+++ src/componentes/page1/index.js
@@ -5,8 +5,6 @@ import View from "./view";
 function Index() {
     const history = useHistory();
     const onClickButton = (name, age) => {
-        console.log('llego ' + name + ' y ' + age);
-        //history.location={pathname:"/page2",state: { nam: "name" }}
         history.push({
             pathname: '/page2',
             state: { name, age }
